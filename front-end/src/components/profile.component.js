d63ab1cb2f93f6cbcbaf1094edfbfcbb270ce90c
Dimitris <dimitris.giokas1@gmail.com>
2020-11-12 22:38:06 +0100

    User Profile page

--- front-end/src/components/profile.component.js
+++ front-end/src/components/profile.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
+import {Tabs, Tab} from 'react-bootstrap-tabs';
 
 class Profile extends Component {
 
@@ -12,28 +13,117 @@ class Profile extends Component {
     }
 
     return (
-      <div className="container">
-        <header className="jumbotron">
-          <h3>
-            <strong>{currentUser.username}</strong> Profile
-          </h3>
-        </header>
-        <p>
-          <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
-          {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-        </p>
-        <p>
-          <strong>Id:</strong> {currentUser.id}
-        </p>
-        <p>
-          <strong>Email:</strong> {currentUser.email}
-        </p>
-        <strong>Authorities:</strong>
-        <ul>
-          {currentUser.roles &&
-            currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-        </ul>
+      <div class="container">
+      <div class="row my-2">
+        <div class="col-lg-8 order-lg-2"></div>
+      <Tabs onSelect={(index, label) => console.log(label + ' selected')}>
+        <Tab label="Profile">
+          <div class="tab-content py-4">
+            <div class="tab-pane active" id="profile">
+                <h5 class="mb-3">User Profile</h5>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h6>Username</h6>
+                          <p>{currentUser.username}</p>
+                        <h6>Email</h6>
+                          <p>{currentUser.email}</p>
+                    </div>
+                    <div class="col-md-12">
+                        <h5 class="mt-2"><span class="fa fa-clock-o ion-clock float-right"></span> My Courses</h5>
+                        <table class="table table-sm table-hover table-striped">
+                            <tbody>                                    
+                                <tr>
+                                    <td>
+                                      <strong>VR for Beginners (Instructor: Anastasia Andreasen)</strong>
+                                    </td>
+                                </tr>
+                                <tr>
+                                  <td>
+                                    <strong>Photoshop for Advanced Learners (Instructor: Anastasia Andreasen)</strong>
+                                  </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </Tab>
+
+        <Tab label="Update">
+          <div class="tab-content py-4">
+            <div class="tab-pane active" id="profile">
+              <h5 class="mb-3">Update</h5>
+                <form>
+                  <div class="form-group row">
+                      <label class="col-md-4 col-form-label form-control-label">First name</label>
+                      <div class="col-md-8">
+                          <input class="form-control" type="text" value="Jane"></input>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-md-4 col-form-label form-control-label">Last name</label>
+                      <div class="col-md-8">
+                          <input class="form-control" type="text" value="Bishop"></input>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-md-4 col-form-label form-control-label">Email</label>
+                      <div class="col-md-8">
+                          <input class="form-control" type="email" value="email@gmail.com"></input>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-md-4 col-form-label form-control-label">Address</label>
+                      <div class="col-md-8">
+                          <input class="form-control" type="text" value="" placeholder="Street"></input>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-md-4 col-form-label form-control-label">Username</label>
+                      <div class="col-md-8">
+                          <input class="form-control" type="text" value="janeuser"></input>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-md-4 col-form-label form-control-label">Password</label>
+                      <div class="col-md-8">
+                          <input class="form-control" type="password" value="11111122333"></input>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-md-4 col-form-label form-control-label">Confirm password</label>
+                      <div class="col-md-8">
+                          <input class="form-control" type="password" value="11111122333"></input>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-md-4 col-form-label form-control-label"></label>
+                    <div class="col-md-8">
+                        <input type="reset" class="btn btn-secondary" value="Cancel"></input>
+                        <input type="button" class="btn btn-primary" value="Save Changes" style={{"margin-left": 10}}></input>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </Tab>
+        
+          <Tab label="Deactivate">
+            <div class="tab-content py-4">
+              <div class="tab-pane active" id="profile">
+                  <h5 class="mb-3">By clicking deactivate your profile will be deleted.</h5>
+                    <div class="row">
+                      <div class="col-md-6">
+                      <button type="button" class="btn btn-danger" style={{"margin-top": 10}}>Deactivate</button>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </Tab>
+        </Tabs>
       </div>
+    </div>    
     );
   }
 }
