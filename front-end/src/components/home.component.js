fbfe5b4de3ec3544bea117b44cb764909c70cdef
rasraziel <rasraziel@gmail.com>
2020-11-13 14:16:18 +0100

    homepage edited

--- front-end/src/components/home.component.js
+++ front-end/src/components/home.component.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 import UserService from "../services/user.service";
-import '../home.css';
 
 export default class Home extends Component {
   constructor(props) {
