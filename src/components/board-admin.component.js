73dd68cc7dbbccac6a673b9c412fc908c62f5528
rasraziel <rasraziel@gmail.com>
2020-12-09 18:00:07 +0100

    orders report done

--- src/components/board-admin.component.js
+++ src/components/board-admin.component.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import CollapsibleTable from "./report-table";
+import CollapsibleTable2 from "./report-table2";
 import UserService from "../services/user.service";
 import MyPiechart from "./piechart";
+import MyPiechart2 from "./piechart2";
 import Footer from "./footer";
 import axios from "../connections";
 import authHeader from "../services/auth-header";
 import "../index.css";
+import { SmsOutlined } from "@material-ui/icons";
 
 export default class BoardAdmin extends Component {
   constructor(props) {
@@ -15,10 +18,9 @@ export default class BoardAdmin extends Component {
       content: "",
       orders: [],
             users: [],
-            userCount:0,
-            adminCount:0,
-            moderatorCount:0, 
-            data:[]
+            data:[],
+            report:"",
+            show:"user"
     };
   }
   
@@ -52,27 +54,64 @@ export default class BoardAdmin extends Component {
 
   }
 
+  reportSelector=(event)=>{
+    this.setState({show:event.target.value});
+
+  }
+
+
   render() {
-    const{users}=this.state;
+    const{users, orders}=this.state;
     const userCount = users.filter(user => user.role === 'ROLE_USER').length;
     const adminCount = users.filter(user => user.role === 'ROLE_ADMIN').length;
     const moderatorCount = users.filter(user => user.role === 'ROLE_MODERATOR').length;
+
+    const javaCount = orders.filter(order => order.courses.includes('Java')).length;
+    const arCount = orders.filter(order => order.courses.includes('Intro to AR')).length;
+    const vrCount = orders.filter(order => order.courses.includes('Intro to VR')).length;
+    const jsCount = orders.filter(order => order.courses.includes('JavaScript')).length;
+    const gitCount = orders.filter(order => order.courses.includes('Git')).length;
+    const angularCount = orders.filter(order => order.courses.includes('Angular')).length;
+    const cCount = orders.filter(order => order.courses.includes('C#')).length;
+    const nodeCount = orders.filter(order => order.courses.includes('NodeJS')).length;
+    const reactCount = orders.filter(order => order.courses.includes('React')).length;
+    const reduxCount = orders.filter(order => order.courses.includes('Redux')).length;
+    const totalCount = javaCount+arCount+vrCount+jsCount+gitCount+angularCount+cCount+nodeCount+reactCount+reduxCount;
+    console.log("I'm java" + javaCount);
       return (
         <div className="grid-container">
             {userCount!==0 && adminCount!==0 && moderatorCount!==0 && <main>
                 <div className="content">
                     <div className="main">
                     <div className="filter">
-                      <span className="reports-title">REPORTS & STATISTICS</span>
-                    </div>  
+                    <div className="filter-sort">
+                    Select report<br></br>  
+                    <select onChange={this.reportSelector}>
+                        <option value="user">User report</option>
+                        <option value="order">Order report</option>
+                    </select>
+                </div>
+                </div>
+
                     </div>
                     
                 </div>
                 <div className="content">
+
+                 {this.state.show==="user"&&userCount!==0 && adminCount!==0 && moderatorCount!==0 && <>
                   <CollapsibleTable/>
                    <div className="chart-container">
                        <MyPiechart userCount={userCount} adminCount={adminCount} moderatorCount={moderatorCount}></MyPiechart>
                     </div>
+                  </>}
+
+                  {this.state.show==="order"&&<>  
+                    <CollapsibleTable2/>
+                   <div className="chart-container">
+                       <MyPiechart2 javaCount={javaCount} totalCount={totalCount} reactCount={reactCount} reduxCount={reduxCount} nodeCount={nodeCount} cCount={cCount} angularCount={angularCount} gitCount={gitCount} jsCount={jsCount} vrCount={vrCount} arCount={arCount}></MyPiechart2>
+                    </div>
+                  </>  }
+
                 </div>
                 
             </main>}
---
+++ src/components/report-table2.jsx
@@ -0,0 +1,219 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from "@material-ui/core/styles";
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import axios from "../connections";
+import authHeader from "../services/auth-header";
+import "../index.css";
+
+
+
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white
+  },
+  body: {
+    fontSize: 12
+  }
+}))(TableCell);
+
+
+const StyledTableRow = withStyles(theme => ({
+  root: {
+    "&:nth-of-type(even)": {
+      backgroundColor: theme.palette.action.hover
+    }
+  }
+}))(TableRow);
+
+const useRowStyles = makeStyles({
+  root: {
+    '& > *': {
+      borderBottom: 'unset',
+    },
+  },
+});
+
+
+
+  function Row(props) {
+    const { row, users } = props;
+    const [open, setOpen] = React.useState(false);
+  
+    return (
+      <React.Fragment>
+        <StyledTableRow>
+          <StyledTableCell align="center">
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          </StyledTableCell>
+          <StyledTableCell component="th" scope="row">{row.order_id}</StyledTableCell>
+          <StyledTableCell align="left">{row.date}</StyledTableCell>
+          <StyledTableCell align="left">{row.total_price_for_courses}</StyledTableCell>
+          <StyledTableCell align="left">{row.courses.map(course => "(" + course + ") ")}</StyledTableCell>
+        </StyledTableRow>
+        <StyledTableRow>
+          <StyledTableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box margin={1}>
+                <Typography variant="h6" gutterBottom component="div">
+                  User data
+                </Typography>
+                <Table size="small" aria-label="purchases">
+                  <TableHead>
+                    <StyledTableRow>
+                      <StyledTableCell>User ID</StyledTableCell>
+                      <StyledTableCell>Username</StyledTableCell>
+                      <StyledTableCell>Email</StyledTableCell>
+                    </StyledTableRow>
+                  </TableHead>
+
+                  <TableBody>
+                  {users.map(user => (
+                    user.user_id===row.user_id&&
+                      <StyledTableRow key={user.user_id}>
+                      <StyledTableCell>{user.user_id}</StyledTableCell>
+                      <StyledTableCell component="th" scope="row">{user.username}</StyledTableCell>
+                      <StyledTableCell>{user.email}</StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+                </TableBody>
+
+                </Table>
+              </Box>
+            </Collapse>
+          </StyledTableCell>
+        </StyledTableRow>
+      </React.Fragment>
+    );
+  }
+
+export default class CollapsibleTable2 extends Component {
+
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          content: "",
+          orders: [],
+          users: [],
+          sort: {
+            column: null,
+            direction: 'desc',
+          }
+        };
+      }
+
+        componentDidMount() {
+            axios
+            .get("reports/orders", { headers: authHeader() })
+            .then((resp) => this.setState({ orders: resp.data }));
+        
+            axios
+            .get("reports/users", { headers: authHeader() })
+            .then((resp) => this.setState({ users: resp.data }));
+        
+        }
+
+        onSort = (column) => (e) => {
+          const direction = this.state.sort.column ? (this.state.sort.direction === 'asc' ? 'desc' : 'asc') : 'desc';
+          const sortedData = this.state.orders.sort((a, b) => {
+          
+            const nameA = a[column];
+            const nameB = b[column];
+            
+              if (nameA < nameB) {
+                return -1;
+              }
+              if (nameA > nameB) {
+                return 1;
+              }
+              // names must be equal
+              return 0;
+          });
+            
+          if (direction === 'desc') {
+            sortedData.reverse();
+          }
+          
+          this.setState({
+            orders: sortedData,
+            sort: {
+              column,
+              direction,
+            }
+          });
+        };
+      
+        setArrow = (column) => {
+          let className = 'sort-direction';
+          
+          if (this.state.sort.column === column) {
+            className += this.state.sort.direction === 'asc' ? ' asc' : ' desc';
+          }
+          
+          console.log(className);
+          
+          return className;
+        };
+
+       
+
+
+        render(){ 
+          const classes = {
+            container: {
+              maxWidth: '40%',
+              boxShadow: 'none',
+              
+          },
+          row:{
+            lineHeight:'0.5rem',
+            cursor: 'pointer'
+          }
+          
+        }
+
+            const{users, orders}=this.state;
+
+            return (
+                <TableContainer style={classes.container} component={Paper}>
+                  <Table aria-label="collapsible table">
+                    <TableHead>
+                      <TableRow >
+                        <StyledTableCell/>
+                        <StyledTableCell style={classes.row} align="left" onClick={this.onSort('order_id')}>Order ID</StyledTableCell>
+                        <StyledTableCell style={classes.row} align="left" onClick={this.onSort('order_date')}>Date</StyledTableCell>
+                        <StyledTableCell style={classes.row} align="left" onClick={this.onSort('total_price_for_courses')}>Total price</StyledTableCell>
+                        <StyledTableCell align="left" >Items</StyledTableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody >
+                      {orders.length>0 && orders.map((row) => (
+                        <Row key={row.order_id} row={row} users={users}/>
+                        
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              );
+        }
+    
+}
