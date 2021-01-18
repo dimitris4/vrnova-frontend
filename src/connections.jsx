38dac6870b748a3a6ee6d62102387ebff348a488
Dimitris <dimitris.giokas1@gmail.com>
2021-01-18 13:43:52 +0100

    aws url

--- src/connections.jsx
+++ src/connections.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // Localhost
-// const baseURL ='http://localhost:8080/'
+const baseURL ='http://localhost:8080/' || 'https://vrnova-backend.herokuapp.com/' || 'http://204.236.221.166:8085/'; 
 
 // Server
-const baseURL ='https://vrnova-backend.herokuapp.com/';
+//const baseURL ='https://vrnova-backend.herokuapp.com/';
 
 export default axios.create({
   baseURL: baseURL,
